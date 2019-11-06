2bd87b35fb303b288c4d8768c3ced4de20dd2a29
Yang Kun <1586500126@qq.com>
2019-11-06 23:50:31 +0800

    首页

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,40 +1,164 @@
 <template>
-    <div class="home">
-        我是首页
-        <div id="main" style="width:500px;height:400px"></div>
+    <div class="home" >
+        首页展示
+        <el-row :gutter="20" style="margin-top:20px;">
+            
+            <el-col :span="8">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>快捷入口</span>
+                    </div>
+                    <el-row :gutter="20">
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-tools"></i>添加
+                            </el-card>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-opportunity"></i>查看
+                            </el-card>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-unfold"></i>进入
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>快捷入口</span>
+                    </div>
+                    <el-row :gutter="20">
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-tools"></i>添加
+                            </el-card>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-opportunity"></i>查看
+                            </el-card>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-unfold"></i>进入
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>快捷入口</span>
+                    </div>
+                    <el-row :gutter="20">
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-tools"></i>添加
+                            </el-card>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-opportunity"></i>查看
+                            </el-card>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-card class="box-card">
+                                <i class="el-icon-s-unfold"></i>进入
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                </el-card>
+            </el-col>
+            <!-- <el-col :span="8">
+                 <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>本月统计</span>
+                    </div>
+                    46532
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>今日统计</span>
+                    </div>
+                    355
+                </el-card>
+            </el-col> -->
+            
+            <el-col :span="8">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>柱状统计</span>
+                    </div>
+                <column width="500px" height="400px"></column>
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                 <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>折线统计</span>
+                    </div>
+                 <linee></linee>
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>饼图统计</span>
+                    </div>
+                 <piee></piee>
+                </el-card>
+            </el-col>
+            
+        </el-row>
     </div>
 </template>
 
 <script>
-import echarts from "echarts"
+// import echarts from "echarts"
+import column from "@/view/echarts/cloumn"
+import linee from "@/view/echarts/line"
+import piee from "@/view/echarts/pie"
 export default {
     data () {
       return {
           
         }
     },
-    mounted () {
-        // var echarts = require('echarts');
-
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('main'));
-        // 绘制图表
-        myChart.setOption({
-            title: {
-                text: 'ECharts 入门示例'
-            },
-            tooltip: {},
-            xAxis: {
-                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        });
+    components:{
+        column,
+        linee,
+        piee
     }
+    // mounted () {
+    //     // var echarts = require('echarts');
+
+    //     // 基于准备好的dom，初始化echarts实例
+    //     var myChart = echarts.init(document.getElementById('main'));
+    //     // 绘制图表
+    //     myChart.setOption({
+    //         title: {
+    //             text: 'ECharts 入门示例'
+    //         },
+    //         tooltip: {},
+    //         xAxis: {
+    //             data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    //         },
+    //         yAxis: {},
+    //         series: [{
+    //             name: '销量',
+    //             type: 'bar',
+    //             data: [5, 20, 36, 10, 10, 20]
+    //         }]
+    //     });
+    // }
 }
 </script>
 
@@ -44,4 +168,5 @@ export default {
     font-size 30px
     lineh 60px
     color red
+   
 </style>
---
+++ src/view/echarts/line.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="linee" style="width:500px ;height:400px">
+    </div>
+</template>
+
+<script>
+import echarts from "echarts"
+export default {
+
+      mounted () {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('linee'));
+        // 绘制图表
+        myChart.setOption({
+             title: {
+                text: 'ECharts 折线'
+            },
+            xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                areaStyle: {}
+            }]
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
